a6e483d74d93f827c8c82dc5b769c1943a7db0d0
DalavaiHemanth <dalavaihemanth2027@gmail.com>
2026-08-07 17:55:33 +0530

    Fix production client initialization by using RouterProvider in entry-client.tsx

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,12 +1,10 @@
-import { hydrateRoot, createRoot } from "react-dom/client";
-import { StartClient } from "@tanstack/react-start";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
 
 const router = getRouter();
 
-const rootElement = document.getElementById("root")!;
-if (rootElement && rootElement.innerHTML) {
-  hydrateRoot(rootElement, <StartClient router={router} />);
-} else if (rootElement) {
-  createRoot(rootElement).render(<StartClient router={router} />);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  createRoot(rootElement).render(<RouterProvider router={router} />);
 }
